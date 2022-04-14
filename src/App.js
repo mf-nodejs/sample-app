fff839d7f440569222aeac4ecbb916bf26fadf01
Andrew Jyan <andrew.jyan@ibm.com>
2022-04-14 12:29:58 -0400

    added confetti

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
+import Confetti from "react-confetti";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Confetti />
         <img
           src={require("./images/tapestryLogo.jpeg")}
           style={{ width: "10em" }}
