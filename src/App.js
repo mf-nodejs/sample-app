63bc3d528041b156b137d63d0c40fe1c4e6512f4
Andrew Jyan <andrew.jyan@ibm.com>
2022-04-14 12:24:15 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Congratulations!</header>
+      <header className="App-header">
+        <img
+          src={require("./images/tapestryLogo.jpeg")}
+          style={{ width: "10em" }}
+          alt="logo"
+        />
+        Congratulations!
+      </header>
     </div>
   );
 }
